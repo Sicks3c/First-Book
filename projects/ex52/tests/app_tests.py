477f34f90614b0508ec713f4de6df39fc6623df9
amiralkizaru <ayoub.elaich.93@gmail.com>
2017-09-27 21:29:20 +0100

    Added exercices

---
+++ projects/ex52/tests/app_tests.py
@@ -0,0 +1,20 @@
+from nose.tools import *
+from bin.app import app
+from tests.tools import assert_response
+
+def test_index():
+    # check that we get a 404 on the / URL
+    resp = app.request("/")
+    assert_response(resp, status="303")
+
+    # test our first GET request to /game
+    resp = app.request("/game")
+    assert_response(resp)
+
+    # make sure default values work for the form
+    resp = app.request("/game", method="POST")
+    assert_response(resp, status="303")
+
+    # test that we get expected values
+    resp = app.request("/game", method="GET")
+    assert_response(resp, status="200")
