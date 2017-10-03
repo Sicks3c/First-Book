0573c24d253cbc08768056d1a130a2277e80862c
amiralkizaru <ayoub.elaich.93@gmail.com>
2017-10-03 21:28:52 +0100

    Check this out

--- useful script/lines.py
+++ useful script/lines.py
@@ -1,4 +1,11 @@
 from sys import argv
 script, filename = argv
-data = open(filename)
-print len(data.readlines())
+infile = open(filename, 'r')
+lines = 0
+characters = 0
+for line in infile:
+    wordslist = line.split()
+    lines = lines + 1
+    characters = characters + len(line)
+print"the lines", lines
+print"the characters", characters
